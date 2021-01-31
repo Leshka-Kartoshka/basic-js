86eebf954ce89c3b5af9c84b97cc46bb172face8
Lesha-Kartoshka <lesha.liho@gmail.com>
2021-01-31 12:54:22 +0300

    feat: carbon-dating.js is done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,13 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+
 
 module.exports = function dateSample(sampleActivity) {
-  var Years;
-  if(typeof eval(sampleActivity) === "number"){
-    Years = Math.log(MODERN_ACTIVITY / sampleActivity) / HALF_LIFE_PERIOD;
-    return Math.ceil(Years);
+  const MODERN_ACTIVITY= 15; 
+  const HALF_LIFE_PERIOD= 5730;
+  var Years = 0;
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  if((parseFloat(sampleActivity) <= 0)){
+    return false;
+  }
+  else if(eval(typeof sampleActivity === "string" && parseFloat(sampleActivity) <= MODERN_ACTIVITY)){
+    Years = Math.ceil(Math.log(MODERN_ACTIVITY / eval(sampleActivity)) / k);
+    return Years;
   }
   else{
     return false;
