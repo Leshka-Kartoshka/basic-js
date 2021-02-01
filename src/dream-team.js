25874aa1c4d151496ad39ae851ae0f0fc733bdef
Lesha-Kartoshka <lesha.liho@gmail.com>
2021-02-01 18:31:45 +0300

    feat: solve dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  var teamName = [];
+  var reg = new RegExp(/\b\w/i);
+  if(!Array.isArray(members)){
+    return false;
+  }
+  else{
+    for(var member of members){
+      if(typeof member == "string"){
+        teamName.push(member.toUpperCase().match(reg));
+      }
+    }
+  }
+  return teamName.sort().join('');
+}
