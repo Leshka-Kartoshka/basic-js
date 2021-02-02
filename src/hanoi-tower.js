db263920d3aec266dae4ba1e5054a39b5c4a282e
Lesha-Kartoshka <lesha.liho@gmail.com>
2021-02-02 18:03:59 +0300

    feat: hanoi tower complite

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,28 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+// function tower(disk)
+// {
+//   if(disk == 1){
+//     steps++;
+//     return steps;
+//   }
+//   steps++;
+//   console.log("step");
+//   tower(disk-1);
+//   tower(disk-1);
+// }
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  var turns = 1;
+  var disks = disksNumber;
+  var hanoiSolution = {
+    turns: 0,
+    seconds: 0
+  }
+  if(disksNumber == 1){
+    turns = 1;
+  }
+  for(var i = 0; i < disksNumber-1; i++){
+   turns = turns * 2 + 1;
+  }
+  hanoiSolution.turns = turns;
+  hanoiSolution.seconds = Math.floor((3600 / turnsSpeed) * hanoiSolution.turns);
+  return hanoiSolution;
 };
