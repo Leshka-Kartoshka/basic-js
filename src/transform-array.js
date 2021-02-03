5df7a4f22d0c964137d47b3f7d5228f3518f0b73
Lesha-Kartoshka <lesha.liho@gmail.com>
2021-02-03 13:12:07 +0300

    feat: transform array solve

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,46 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  var newarr = [];
+  if(Array.isArray(arr)){
+    if(arr.length != 0){
+      for(var i = 0, m = 0; i < arr.length; i++, m++){
+        if(typeof arr[i] === "string"){
+          switch(arr[i]){
+            case "--discard-next":
+              if(i < arr.length){
+                i++;
+              }
+              break;
+            case "--discard-prev":
+              if(i > 0){
+                newarr.splice(i-1, 1);
+                m--;
+              }
+              break;
+            case "--double-next":
+              if(i < arr.length){
+                newarr.push(arr[i+1]);
+              }
+              break;
+            case "--double-prev":
+              if(i > 0){
+                newarr.push(arr[i-1]);
+              }
+              break;
+          }
+        }
+        else{
+          newarr.push(arr[i]);
+        }
+      }
+    }
+  }else{
+    throw new Error("arr is not an array");
+  }
 };
+
+
+/*
+
+
+`
+`*/
