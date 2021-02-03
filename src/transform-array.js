5e89c356cc9d45ab5a5670eaac69e52fdec17353
Lesha-Kartoshka <lesha.liho@gmail.com>
2021-02-03 13:29:07 +0300

    feat: transform array solve

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,44 +3,38 @@ module.exports = function transform(arr) {
   if(Array.isArray(arr)){
     if(arr.length != 0){
       for(var i = 0, m = 0; i < arr.length; i++, m++){
-        if(typeof arr[i] === "string"){
-          switch(arr[i]){
-            case "--discard-next":
-              if(i < arr.length){
-                i++;
-              }
-              break;
-            case "--discard-prev":
-              if(i > 0){
-                newarr.splice(i-1, 1);
-                m--;
-              }
-              break;
-            case "--double-next":
-              if(i < arr.length){
-                newarr.push(arr[i+1]);
-              }
-              break;
-            case "--double-prev":
-              if(i > 0){
-                newarr.push(arr[i-1]);
-              }
-              break;
-          }
-        }
-        else{
-          newarr.push(arr[i]);
+        switch(arr[i]){
+          case undefined:
+            break;
+          case "--discard-next":
+            if(i < arr.length){
+              i++;
+            }
+            break;
+          case "--discard-prev":
+            if(i > 0 && arr[i-1] != undefined && arr[i-2] != "--discard-next"){
+            newarr.pop();
+            m--;
+            }
+            break;
+          case "--double-next":
+            if(i < arr.length && arr[i+1] != undefined){
+              newarr.push(arr[i+1]);
+            }
+            break;
+          case "--double-prev":
+            if(i > 0 && arr[i-1] != undefined && arr[i-2] != "--discard-next"){
+              newarr.push(arr[i-1]);
+            }
+            break;
+          default:
+            newarr.push(arr[i]);
+            break;
         }
       }
     }
   }else{
     throw new Error("arr is not an array");
   }
+  return newarr;
 };
-
-
-/*
-
-
-`
-`*/
