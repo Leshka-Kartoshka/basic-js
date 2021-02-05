ec75bedf2e5f79be4dc5ff62fc5757de62577332
Lesha-Kartoshka <lesha.liho@gmail.com>
2021-02-05 17:53:33 +0300

    feat: extended repeater complite

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,23 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+  var newstr = [];
+  var substring = [];
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  (options.separator != undefined)?options.separator:options.separator = '+';
+  (options.additionSeparator != undefined)?options.additionSeparator:options.additionSeparator = '|';
+  (options.additionRepeatTimes >= 1)?options.additionRepeatTimes:options.additionRepeatTimes = 1;
+  (options.repeatTimes >= 1)?options.repeatTimes:options.repeatTimes = 1;
+  (options.addition !== undefined)?options.addition:options.addition = '';
+
+  if(options.additionRepeatTimes >= 1){
+    for(var i = 0; i < options.additionRepeatTimes; i++){
+      substring.push(options.addition + '');
+    }
+    substring = substring.join(options.additionSeparator);console.log(options.addition);
+  }
+  for(var i = 0; i < options.repeatTimes; i++){
+      newstr.push(str + substring);
+  }
+  newstr = newstr.join(options.separator);
+  return newstr;
+  };
   
